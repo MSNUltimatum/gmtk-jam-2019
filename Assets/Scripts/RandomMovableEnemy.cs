2eda5d29a3fb8e872aa0dec74ae6a9cff2b782bb
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2019-08-04 01:25:42 +0400

    Break Enemy

---
+++ Assets/Scripts/RandomMovableEnemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovableEnemy : MonoBehaviour
+{
+    [SerializeField]
+    protected float EnemySpeed = 2f;
+    Vector3 direct;
+    private float CoolDownBefore;
+    private float CoolDown = 0.25f;
+    private SpriteRenderer sprite;
+    protected GameObject Player;
+
+    protected void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        CoolDownBefore = CoolDown;
+        direct = Player.transform.position;
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    protected  void Update()
+    {
+        CoolDownBefore = Mathf.Max(CoolDownBefore - Time.deltaTime, 0);
+        NewDirect();
+        MoveToward();
+        Rotation();
+    }
+
+    protected  void MoveToward()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, direct, EnemySpeed * Time.deltaTime);
+    }
+
+    protected  void Rotation()
+    {
+        float z = Mathf.Atan2((Player.transform.position.y - transform.position.y), (Player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+       sprite.transform.eulerAngles = new Vector3(0, 0, z);
+    }
+
+    private void NewDirect()
+    {
+        if (CoolDownBefore == 0) { 
+            var vect = Player.transform.position + direct / 2;
+        vect.Normalize();
+        vect.x += Random.Range(-1f, 1f);
+        vect.y += Random.Range(-1f, 1f);
+        vect.Normalize();
+        direct = vect;
+        CoolDownBefore = CoolDown;
+        }
+        
+    }
+}
--- Assets/Scripts/ShootableMonster.cs
+++ Assets/Scripts/ShootableMonster.cs
@@ -41,7 +41,9 @@ public class ShootableMonster : EnemyMovement
         var bullet = Instantiate(Bullet, transform.position, new Quaternion());
 
         var offset = new Vector2(PlayerPos.x - transform.position.x, PlayerPos.y - transform.position.y);
+        
         var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        angle += Random.Range(-15, 15);
         bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
         bullet.transform.Translate(Vector2.right * 0.5f);
     }
