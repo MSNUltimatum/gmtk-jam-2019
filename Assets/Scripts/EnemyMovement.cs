1a8deccba1ddc675f152d6d0928970e2f89344a2
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2019-08-03 21:17:31 +0400

    teleport enemys

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,61 +5,31 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
-    private float EnemySpeed = 2f;
-
-    private GameObject Player;
+    protected float EnemySpeed = 2f;
+    protected GameObject Player;
     private SpriteRenderer sprite;
 
-    private float CoolDownBefore;
-
-    private void Start()
+    protected virtual void Start()
     {
-        CoolDownBefore = Rand();
         sprite = GetComponentInChildren<SpriteRenderer>();
         Player = GameObject.FindGameObjectWithTag("Player");
-
-        var offset = Player.transform.position - transform.position;
-        var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle + 180);
     }
 
-    private void Update()
+    protected virtual void Update()
     {
-        CoolDownBefore = Mathf.Max(CoolDownBefore - Time.deltaTime , 0);
         MoveToward();
         Rotation();
 
     }
 
-    private void MoveToward()
+    protected virtual void MoveToward()
     {
-        ExtraSpeed();
         transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, EnemySpeed * Time.deltaTime);
     }
 
-    private void Rotation()
+    protected virtual void Rotation()
     {
-        var offset = Player.transform.position - transform.position;
-        var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle + 180);
-    }
-
-    private void ExtraSpeed()
-    {
-        if (CoolDownBefore < 2f)
-        {
-            EnemySpeed = 3.5f;
-        }
-
-        if(CoolDownBefore == 0)
-        {
-            CoolDownBefore = Rand();
-            Debug.Log(CoolDownBefore);
-            EnemySpeed = 2f;
-        }
-    }
-    private float Rand()
-    {
-        return Random.Range(4f, 7f);
+        float z = Mathf.Atan2((Player.transform.position.y - transform.position.y), (Player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+        sprite.transform.eulerAngles = new Vector3(0, 0, z);
     }
 }
