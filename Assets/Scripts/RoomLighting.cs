106d725462905c1567d27fa4ea3049ec7dd1658b
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2019-08-04 15:09:48 +0400

    lighting errors fixed

--- Assets/Scripts/RoomLighting.cs
+++ Assets/Scripts/RoomLighting.cs
@@ -10,6 +10,9 @@ public class RoomLighting : MonoBehaviour
     private ArenaEnemySpawner arena;
     private float TotalValue = 0;
     private float maxvalue = 0;
+    private float CurrentVal;
+    static float t = 0.0f;
+    static float Light;
 
     private void Start()
     {
@@ -17,19 +20,30 @@ public class RoomLighting : MonoBehaviour
         maxvalue = arena.EnemyCount();
         GameObject TileMap = GameObject.FindGameObjectWithTag("TailMap");
         Tile = TileMap.transform.GetChild(0).gameObject;
+        Light = 0.2f + (TotalValue / maxvalue) * 0.8f;
         tilemap = Tile.GetComponent<Tilemap>();
-        NewLight();
+        NewLight(Light);
     }
 
     public void Lighten(float val)
     {
         TotalValue = TotalValue + val;
-        NewLight();
+         Light = 0.2f + (TotalValue / maxvalue) * 0.8f;
+        t = 0.0f;
     }
 
-    private void NewLight()
+    private void Update()
     {
-        float Light = 0.2f + (TotalValue / maxvalue) * 0.8f;
+        if (t < 0.7f)
+        {
+            CurrentVal = Mathf.Lerp(tilemap.color.g, Light, t);
+            NewLight(CurrentVal);
+        }
+        
+        t += Time.deltaTime;
+    }
+    private void NewLight(float Light)
+    {
         tilemap.color = new Color(Light, Light, Light);
     }
 }
