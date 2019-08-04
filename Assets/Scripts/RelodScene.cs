4db24d239ca784d4e1cd8aee1be72981b17dd6a2
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2019-08-04 15:57:09 +0400

    Reload Scenes

---
+++ Assets/Scripts/RelodScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RelodScene : MonoBehaviour
+{
+    private ArenaEnemySpawner arena;
+    private float TotalValue = 0;
+    private float maxvalue = 0;
+
+    private void Start()
+    {
+        arena = GetComponent<ArenaEnemySpawner>();
+        maxvalue = arena.EnemyCount();
+    }
+
+    public void CurrentCount(int val)
+    {
+        TotalValue = TotalValue + val;
+    }
+
+    private void Update()
+    {
+        if(TotalValue == maxvalue)
+        {
+            SceneManager.LoadScene("NewMsnScene");
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
