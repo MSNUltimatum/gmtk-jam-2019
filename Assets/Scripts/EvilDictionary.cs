2839d785b751c3cc52de80cffda3768fb60540ef
Nikita Vasin(notebook) <nikitavsn64rus@gmail.com>
2019-08-08 22:43:27 +0400

    EvilDictionary updated

--- Assets/Scripts/EvilDictionary.cs
+++ Assets/Scripts/EvilDictionary.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class EvilDictionary : MonoBehaviour
 {
-    // If you'd like to add more names, better use
-    // component hierarchy and set your own EvilNames array
     public readonly string[] EvilNames = {
-        "Unemployment", "Fear",
-        "Who am I?", "Corruption", "Doubt",
-        "Betrayal", "Fall", "Not for me",
-        "Envy", "Doom", "Depression", "Dejection",
-        "Misery", "Agony", "Excruciation",
-        "Laceration", "Grief", "Sorrow", "Desolation",
-        "Dolor", "Breakdown", "Loneliness", "Cheerless",
-        "Mirthless", "Dull", "Madness", "Desperation",
-        "Insanity", "Fury", "Wrath", "Enragement", "Frustration"
+        "Abomination", "Abort", "Abuse", "Acrimonious", "Aggression", "Agony",
+        "Banal", "Bereavement", "Bitchy", "Blame", "Brainless", "Betrayal", "Breakdown",
+        "Catastrophe", "Chaos", "Conceit", "Conflict", "Crime", "Corruption", "Cheerless",
+        "Danger", "Darkness", "Deceiving", "Defect", "Derision", "Doubt", "Doom",
+        "Dull", "Dolor", "Desperation", "Depression", "Dejection",
+        "Egotism", "Epidemic", "Evil", "Exhausted", "Extremism", "Enragement", "Excruciation", "Envy",
+        "Fail", "Fake", "Fatal", "Fear", "Filth", "Fury", "Fall", "Frustration",
+        "Gainsay", "Gloomy", "Grind", "Grumpy", "Grudge", "Grief",
+        "Harass", "Harmful", "Hate", "Havoc", "Horrid",
+        "Idiotic", "Ignore", "Impiety", "Impersonal", "Impolite", "Insanity",
+        "Jaded", "Jarring", "Jeer", "Junky", "Jeopardize",
+        "Kaput", "Killing", "Killjoy", "Kook", "Kooky",
+        "Lack", "Lackadaisical", "Lag", "Lament", "Lawless", "Loneliness", "Laceration",
+        "Mad", "Malaise", "Malign", "Manipulate", "Marginal", "Misery", "Madness", "Mirthless",
+        "Nag", "Negation", "Nervous", "Niggles", "Nonsense", "Not for me",
+        "Objection", "Obese", "Obscene", "Obstruct", "Omission",
+        "Pain", "Panic", "Paranoia", "Peeve", "Polution",
+        "Qualms", "Quandary", "Quarrel", "Questionable", "Quitter",
+        "Rabid", "Racism", "Rage", "Rascal", "Repugnant",
+        "Sad", "Sass", "Scam", "Scare", "Selfish", "Sorrow",
+        "Tantrum", "Tease", "Terrible", "Terror", "Thwart",
+        "Ugly","Uncaring", "Uncivil", "Unemployment", "Underdog",
+        "Vague", "Vain", "Venomous", "Vex", "Violent",
+        "Wack", "Wail", "Wasted", "Wrath", "Weak", "Wicked", "Who am I?",
+        "Zap", "Zealot", "Zealous", "Zealously", "Zombie"
     };
 }
