73e4c325f2a2dcffb5fd838e4bc6cc7307173a99
Nikita Vasin(notebook) <nikitavsn64rus@gmail.com>
2019-08-09 13:46:19 +0400

    Dictionary fixed

--- Assets/Scripts/EvilDictionary.cs
+++ Assets/Scripts/EvilDictionary.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class EvilDictionary : MonoBehaviour
 {
     public readonly string[] EvilNames = {
-        "Abomination", "Abort", "Abuse", "Acrimonious", "Aggression", "Agony",
+        "Abomination", "Abuse", "Acrimonious", "Aggression", "Agony",
         "Banal", "Bereavement", "Bitchy", "Blame", "Brainless", "Betrayal", "Breakdown",
         "Catastrophe", "Chaos", "Conceit", "Conflict", "Crime", "Corruption", "Cheerless",
         "Danger", "Darkness", "Deceiving", "Defect", "Derision", "Doubt", "Doom",
         "Dull", "Dolor", "Desperation", "Depression", "Dejection",
-        "Egotism", "Epidemic", "Evil", "Exhausted", "Extremism", "Enragement", "Excruciation", "Envy",
+        "Egotism", "Epidemic", "Evil", "Exhausted", "Enragement", "Excruciation", "Envy", "Enslave",
         "Fail", "Fake", "Fatal", "Fear", "Filth", "Fury", "Fall", "Frustration",
         "Gainsay", "Gloomy", "Grind", "Grumpy", "Grudge", "Grief",
         "Harass", "Harmful", "Hate", "Havoc", "Horrid",
         "Idiotic", "Ignore", "Impiety", "Impersonal", "Impolite", "Insanity",
-        "Jaded", "Jarring", "Jeer", "Junky", "Jeopardize",
+        "Jaded", "Jarring", "Jeer", "Jobless", "Jittery",
         "Kaput", "Killing", "Killjoy", "Kook", "Kooky",
-        "Lack", "Lackadaisical", "Lag", "Lament", "Lawless", "Loneliness", "Laceration",
+        "Lack", "Lackadaisical", "Lawless", "Loneliness", "Laceration",
         "Mad", "Malaise", "Malign", "Manipulate", "Marginal", "Misery", "Madness", "Mirthless",
         "Nag", "Negation", "Nervous", "Niggles", "Nonsense", "Not for me",
         "Objection", "Obese", "Obscene", "Obstruct", "Omission",
@@ -25,10 +25,10 @@ public class EvilDictionary : MonoBehaviour
         "Qualms", "Quandary", "Quarrel", "Questionable", "Quitter",
         "Rabid", "Racism", "Rage", "Rascal", "Repugnant",
         "Sad", "Sass", "Scam", "Scare", "Selfish", "Sorrow",
-        "Tantrum", "Tease", "Terrible", "Terror", "Thwart",
+        "Tantrum", "Tease", "Terrible", "Tired", "Thwart",
         "Ugly","Uncaring", "Uncivil", "Unemployment", "Underdog",
         "Vague", "Vain", "Venomous", "Vex", "Violent",
         "Wack", "Wail", "Wasted", "Wrath", "Weak", "Wicked", "Who am I?",
-        "Zap", "Zealot", "Zealous", "Zealously", "Zombie"
+        "Zap", "Zealous", "Zealously"
     };
 }
