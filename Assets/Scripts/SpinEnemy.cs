07cc15219336a05d240c45746aab23303e62ec71
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2019-08-15 15:56:15 +0400

    Spin Enemy

---
+++ Assets/Scripts/SpinEnemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinEnemy : EnemyMovement
+{
+    [SerializeField]
+    private float angle = 0f;
+    private bool isCircle = false;
+    private float NotSpinSpeed;
+
+    [SerializeField]
+    private float radius = 2f;
+    protected override void Start()
+    {
+        base.Start();
+        NotSpinSpeed = EnemySpeed;
+    }
+
+    protected override void Update()
+    {
+        MoveToward();
+        Spin();
+        Rotation();
+    }
+
+    protected override void MoveToward()
+    {
+        if (Vector3.Distance(Player.transform.position, transform.position) > radius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, EnemySpeed * Time.deltaTime);
+            isCircle = false;
+            //angle = 0f; При этом условии он стоит
+            EnemySpeed = NotSpinSpeed;
+        }
+        else
+        {
+            isCircle = true;
+        }
+    }
+
+    private void Spin()
+    {
+        if (isCircle)
+        {
+            EnemySpeed = NotSpinSpeed + 2f ;
+            angle += Time.deltaTime;
+            var x = Mathf.Cos(angle * EnemySpeed) * radius;
+            var y = Mathf.Sin(angle * EnemySpeed) * radius;
+            transform.position = new Vector3(x, y, transform.position.z) + Player.transform.position;
+        }
+    }
+
+    protected override void Rotation()
+    {
+        base.Rotation();
+    }
+}
