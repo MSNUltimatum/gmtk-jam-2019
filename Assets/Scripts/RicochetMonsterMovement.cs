45dfe4d6ec7b85ae68bb4ef1f2287124bc4a358e
Alexflames <alexprgrigoriev@gmail.com>
2019-08-23 20:50:49 +0400

    ricochet monster prototype (bug when shooted out of bounds)

---
+++ Assets/Scripts/RicochetMonsterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RicochetMonsterMovement : EnemyMovement
+{
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        direction = (Player.transform.position - gameObject.transform.position).normalized;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        transform.Translate(direction * EnemySpeed * Time.deltaTime);
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Environment")
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction,
+                    float.PositiveInfinity, LayerMask.GetMask("Default"));
+            if (hit)
+            {
+                direction = Vector2.Reflect(direction, hit.normal);
+            }
+        }
+    }
+}
