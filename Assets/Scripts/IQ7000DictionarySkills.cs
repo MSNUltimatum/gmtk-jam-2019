657b8be487e56a042439b7866cc296bf89f1dbd3
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2019-09-01 23:36:19 +0400

    Skills

---
+++ Assets/Scripts/IQ7000DictionarySkills.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IQ7000DictionarySkills : MonoBehaviour
+{
+    public static Dictionary<string, SkillBase> skills = new Dictionary<string, SkillBase>();
+    private void Start()
+    {
+        WeirdPill Pill = new WeirdPill();
+        skills.Add("SpeedAura", Pill);
+    }
+
+    public static SkillBase GetValue(string Name)
+    {
+        foreach(KeyValuePair<string, SkillBase> keyValue in skills)
+        {
+            if (keyValue.Key == Name)
+                return keyValue.Value;
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/PlayerSkills.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerSkills : MonoBehaviour
+{
+    public List<SkillBase> skills = new List<SkillBase>();
+    private void Start()
+    {
+        PlayerPrefs.SetString("SkillString", "SpeedAura ");
+        if(PlayerPrefs.HasKey("SkillString"))
+        {
+            string AllSkills = PlayerPrefs.GetString("SkillString");
+            string[] skillsStr = AllSkills.Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries);
+            foreach (var i in skillsStr)
+            {
+                skills.Add(IQ7000DictionarySkills.GetValue(i));
+            }
+            foreach (var i in skills)
+            {
+                if(i is PassiveSkill)
+                    i.ActiAcquireSkill();  
+            }
+        }
+
+    }
+}
