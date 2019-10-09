c1bc67fede4129c9c218188fcf71153992a386bf
Yegor Kondratyev <evkondratyev@yandex.ru>
2019-10-09 12:36:35 +0400

    Removed unnecessary namespace inclusions.

--- Assets/Scripts/Enemy Behaviors/Seek.cs
+++ Assets/Scripts/Enemy Behaviors/Seek.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Seek : EnemyBehavior
+﻿public class Seek : EnemyBehavior
 {
     public override EnemySteering GetSteering()
     {
