c4252f8beab510b7b2f58f926cdf149d510d951b
Yegor Kondratyev <evkondratyev@yandex.ru>
2019-10-09 12:24:52 +0400

    Added strafing to the zombie enemy.

---
+++ Assets/Scripts/Enemy Behaviors/SeekAndStrafe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekAndStrafe : Seek
+{
+    public override EnemySteering GetSteering()
+    {
+        EnemySteering steering = base.GetSteering();
+        var quat = new Quaternion();
+        quat.eulerAngles = new Vector3(0, 0, dirChangeAngle);
+        steering.linear = quat * steering.linear;
+
+        return steering;
+    }
+
+    public override void Update()
+    {
+        if (dirChangeTimer <= 0)
+        {
+            dirChangeAngle = Random.Range(-dirChangeMaxAngle, dirChangeMaxAngle);
+            dirChangeTimer = dirChangeTimeMax;
+        }
+        dirChangeTimer -= Time.deltaTime;
+        base.Update();
+    }
+
+    private float dirChangeTimer = 0.0f;
+    private float dirChangeTimeMax = 2.0f;
+    private float dirChangeAngle = 0.0f;
+    private float dirChangeMaxAngle = 50.0f;
+}
