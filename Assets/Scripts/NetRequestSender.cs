8509d2bafed9826164220bcc355d12cc162ad294
Yegor Kondratyev <evkondratyev@yandex.ru>
2019-10-27 12:36:06 +0400

    Added net request sender template with an example.

---
+++ Assets/Scripts/NetRequestSender.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetRequestSender : MonoBehaviour
+{
+    protected IEnumerator GetRequest(string uri)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(uri);
+        yield return webRequest.SendWebRequest();
+
+        DebugLogNetworkRequest(webRequest);
+    }
+
+    protected IEnumerator PostRequest(string url, WWWForm form)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Post(url, form);
+        yield return webRequest.SendWebRequest();
+
+        DebugLogNetworkRequest(webRequest);
+    }
+
+    protected IEnumerator PostRequestJson(string url, string json)
+    {
+        var webRequest = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+
+        yield return webRequest.SendWebRequest();
+
+        DebugLogNetworkRequest(webRequest);
+    }
+
+    private void DebugLogNetworkRequest(UnityWebRequest webRequest)
+    {
+        if (webRequest.isNetworkError || webRequest.isHttpError)
+        {
+            Debug.Log("Error: " + webRequest.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + webRequest.downloadHandler.text);
+        }
+    }
+}
