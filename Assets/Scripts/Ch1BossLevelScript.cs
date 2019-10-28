cf2d2c1817bc9fd7d392d6acd384cc7c054325ea
Alexflames <alexprgrigoriev@gmail.com>
2019-10-28 21:09:24 +0400

    small logic changes to intro

--- Assets/Scripts/Ch1BossLevelScript.cs
+++ Assets/Scripts/Ch1BossLevelScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Ch1BossLevelScript : MonoBehaviour
 {
+    private Transform camera;
     enum Phase
     {
+        INACTIVE,
         INTRO,
-
     }
 
     // Start is called before the first frame update
@@ -16,16 +17,39 @@ public class Ch1BossLevelScript : MonoBehaviour
     private void Start()
     {
         CurrentEnemy.SetCurrentEnemyName("???");
+        camera = Camera.main.transform;
     }
 
     private void Update()
     {
         switch (CurrentPhase)
         {
+            case Phase.INACTIVE:
+                break;
             case Phase.INTRO:
                 break;
             default:
                 break;
         }
+    }
+
+    private void LateUpdate()
+    {
+        switch (CurrentPhase)
+        {
+            case Phase.INTRO:
+                CameraShakeUpdate();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private float shakeDuration = 0;
+    private float shakeAmount = 0.7f;
+
+    private void CameraShakeUpdate()
+    {
+        
     }
 }
