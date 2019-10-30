37cd0179f053e31bfefaa41469d86cde4c97cab5
Nikita Vasin(notebook) <nikitavsn64rus@gmail.com>
2019-10-30 23:49:22 +0400

    mainmenu logic

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
@@ -42,6 +43,10 @@ public class Pause : MonoBehaviour
         {
             SetPause(!Paused);
         }
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
     }
 
     private static Transform myTransform;
