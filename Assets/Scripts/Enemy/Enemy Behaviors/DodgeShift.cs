07ce3f8cedec790e437506d54daf66950aa9875a
Artaniel <59449523+Artaniel@users.noreply.github.com>
2020-01-04 17:16:56 +0200

    add dodge to magic monster
    added script for dodge behaviour
    added flag for shoot frame to CharacterShooting sctipt
    changed prefab for magic monster

---
+++ Assets/Scripts/Enemy/Enemy Behaviors/DodgeShift.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeShift : EnemyBehavior
+{
+    private CharacterShooting shotScript;
+    //private bool dodgeMode = false;
+    Vector2 dodgeDirrection;
+    public float shiftAmp = 1f;
+    private Rigidbody2D rigidB;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        shotScript = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterShooting>();
+        rigidB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public override void CalledUpdate()
+    {
+        base.CalledUpdate();
+        
+        if (shotScript.shotFrame) {
+            float angle = Random.Range(0f, 360f);
+            dodgeDirrection = new Vector2( shiftAmp * Mathf.Sin(angle * Mathf.PI / 180f), shiftAmp * Mathf.Cos(angle * Mathf.PI / 180f));
+            if (rigidB != null)
+            {
+                rigidB.AddForce(dodgeDirrection, ForceMode2D.Impulse);
+            }
+            else {
+                Debug.Log("Error, no Rigidbody2D on monster");
+            }
+        }
+    }
+}
