1629f69d025c1c1c61c441b1a6bcf246e3a8526a
Sadpotato <alexprgrigoriev@gmail.com>
2020-01-04 13:12:33 +0400

    sound refactoring, part 1

---
+++ Assets/Scripts/SFX/PlaySoundOnAwake.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnAwake : MonoBehaviour
+{
+    [SerializeField]
+    private bool shouldPlaySound = false;
+    
+    [Header("Non-default name?")]
+    [Tooltip("Can be left blank if there is no need in specific name")]
+    [SerializeField]
+    private string clipName = "";
+
+    void Awake()
+    {
+        if (shouldPlaySound)
+        {
+            var source = GetComponent<AudioSource>();
+            var name = clipName == "" ? source.clip.name : clipName;
+            AudioManager.Play(name, source);
+        }    
+    }
+}
