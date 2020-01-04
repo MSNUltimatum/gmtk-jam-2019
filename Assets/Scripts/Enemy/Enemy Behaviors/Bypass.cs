6ec4f350b63d0a2f7705ca3dd42d2a9958ced4af
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2020-01-04 14:19:08 +0400

    Funny, but useless

--- Assets/Scripts/Enemy/Enemy Behaviors/Bypass.cs
+++ Assets/Scripts/Enemy/Enemy Behaviors/Bypass.cs
@@ -6,16 +6,16 @@ using System.Linq;
 public class Bypass : Align
 {
     [SerializeField]
-    protected float maxRaycastDistance = 2.5f;
+    protected float maxRaycastDistance = 2f;
 
     public override EnemySteering GetSteering()
     {
-        Vector2 direction = target.transform.position - transform.position;
-        var hits = RaycastHits(direction.normalized);
+        direction = target.transform.position - transform.position;
+        var hits = RaycastHits(direction);
         hits = (from t in hits
                 where t.transform.gameObject.tag == "Environment" || t.transform.gameObject.tag == "Player"
                 select t).ToArray();
-        if (hits.Length == 0 || hits[0].transform.gameObject.tag == "Player")
+        if(hits.Length == 0 || hits[0].transform.gameObject.tag == "Player")
         {
             if (direction.magnitude > 0.0f)
             {
@@ -27,34 +27,30 @@ public class Bypass : Align
         }
         else
         {
-            if (direction.magnitude > 0.0f)
-            {
-                var selectItems = from t in hits
-                                  where t.transform.gameObject.tag == "Environment"
-                                  select t;
-                var environments = selectItems.ToArray();
-                if (environments.Length > 0)
-                {
-                    float targetOrientation = Mathf.Atan2(direction.x, direction.y);
-                    targetOrientation *= Mathf.Rad2Deg;
-                    Vector2 direct = Vector2.Reflect(direction.normalized, environments[0].normal);
-                    float rot = Mathf.Atan2(direct.x, direct.y) * Mathf.Rad2Deg;
-                    targetOrientation += rot * Time.deltaTime * 53;
-                    Debug.Log(targetOrientation);
-                    targetOrientation %= 360.0f;
-                    if (targetOrientation < 0.0f)
-                    {
-                        targetOrientation += 360.0f;
-                    }
-                    base.targetOrientation = targetOrientation;
-                }
-            }
+            var environment = (from t in hits
+                                where t.transform.gameObject.tag == "Environment"
+                                select t).ToArray();
+            direct = (target.transform.position - transform.position).normalized;
+            direct += environment[0].normal * 2;
+            Quaternion rot = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direct), 2 * Time.deltaTime);
+            rot.x = 0;
+            rot.y = 0;
+            targetOrientation += rot.z * Mathf.Rad2Deg * Time.deltaTime;
+            base.targetOrientation = targetOrientation;
             return base.GetSteering();
         }
     }
 
-    private RaycastHit2D[] RaycastHits(Vector2 direction)
+    private RaycastHit2D[] RaycastHits(Vector2 direction1)
     {
+        //Debug.DrawLine(transform.position, direction1.normalized);
+        Debug.DrawRay(transform.position, direction1.normalized * maxRaycastDistance, Color.green);
         return Physics2D.RaycastAll(transform.position, direction, maxRaycastDistance);
     }
+
+    private bool isWall = false;
+    Vector2 direction;
+    Vector2 direct;
+    RaycastHit2D newTarget = new RaycastHit2D();
+    float offset;
 }
