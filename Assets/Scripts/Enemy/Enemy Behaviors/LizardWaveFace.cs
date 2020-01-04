b069f4d524663c3ebd4a1ab71cd908c44a671157
Artaniel <59449523+Artaniel@users.noreply.github.com>
2020-01-04 17:11:10 +0200

    add reptile wave move
    added script for reptile wave move
    changed reptile prefab

---
+++ Assets/Scripts/Enemy/Enemy Behaviors/LizardWaveFace.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LizardWaveFace : Align
+{
+    private float wavePhase = 0f; 
+    public float wavePeriod = 2f; 
+    public float waveAmp = 40f; 
+
+    public override EnemySteering GetSteering()
+    {
+        Vector2 direction = target.transform.position - transform.position;
+        if (direction.magnitude > 0.0f) 
+        {
+            float targetOrientation = Mathf.Atan2(direction.x, direction.y);
+            targetOrientation *= Mathf.Rad2Deg;
+            targetOrientation += WaveFluctuation();
+            base.targetOrientation = targetOrientation;
+        }
+
+        return base.GetSteering();
+    }
+
+    private float WaveFluctuation() {//delta orientation from line of sight to wave trajectory
+        float fluctuation = 0f;
+        wavePhase += Time.deltaTime / wavePeriod;
+        if (wavePhase > 1) wavePhase -= 1;
+        fluctuation = Mathf.Sin(wavePhase * 2 * Mathf.PI) * waveAmp;
+        return fluctuation;
+    }
+
+}
