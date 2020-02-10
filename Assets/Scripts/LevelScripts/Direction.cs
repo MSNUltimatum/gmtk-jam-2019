4f441da1a69ff6f4461abb9c25ffc6b23f5d7724
Sadpotato <alexprgrigoriev@gmail.com>
2020-02-10 19:50:29 +0400

    Direction class for 4 axis (rooms & doors)

---
+++ Assets/Scripts/LevelScripts/Direction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Direction
+{
+    public enum Side
+    {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT
+    }
+
+    public static Side[] Sides()
+    {
+        return new Side[] { Side.DOWN, Side.LEFT, Side.RIGHT, Side.UP };
+    }
+
+    public static Side InvertSide(Side side)
+    {
+        switch (side)
+        {
+            case Side.UP:
+                return Side.DOWN;
+            case Side.RIGHT:
+                return Side.LEFT;
+            case Side.DOWN:
+                return Side.UP;
+            case Side.LEFT:
+                return Side.RIGHT;
+            default:
+                return Side.UP;
+        }
+    }
+}
