c060baa88cd61f950f7567c03d7e05a5591495cd
Sadpotato <alexprgrigoriev@gmail.com>
2020-02-23 15:47:34 +0400

    Fps meter and camera after reload scene fix

---
+++ Assets/Scripts/UI/FPSMeter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSMeter : MonoBehaviour
+{
+    private Text text;
+    private float fpsSum = 0;
+    private uint calculations = 0;
+    private float fpsTimer;
+    
+    void Start()
+    {
+        text = GetComponent<Text>();    
+    }
+
+    void Update()
+    {
+        fpsTimer += Time.deltaTime;
+        if (fpsTimer >= 1)
+        {
+            fpsTimer = 0;
+            calculations++;
+            var currentFPS = 1 / Time.deltaTime;
+            fpsSum += currentFPS;
+            text.text = $"FPS: {(currentFPS).ToString("0.00")}. Average: { (fpsSum / calculations).ToString("0.00") }";
+        }
+        
+    }
+}
