0754450f0b553a529fe8f96ceb527665adbeba2d
Sadpotato <alexprgrigoriev@gmail.com>
2020-03-08 21:55:18 +0400

    Circlular bullet explosion mod

---
+++ Assets/Scripts/Skills/Passive/CircularStrikeOnDeathSkill.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FourWayStrikeOnDeathSkill", menuName = "ScriptableObject/PassiveSkill/FourWayStrikeOnDeath", order = 1)]
+public class CircularStrikeOnDeathSkill : PassiveSkill
+{
+    
+}
