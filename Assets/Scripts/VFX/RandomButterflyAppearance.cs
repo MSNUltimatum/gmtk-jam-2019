8f63a95f75f2fd4e8cd1d0db54de6ccd8f751cea
Sadpotato <alexprgrigoriev@gmail.com>
2020-03-19 21:25:20 +0400

    butterflies included

---
+++ Assets/Scripts/VFX/RandomButterflyAppearance.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomButterflyAppearance : MonoBehaviour
+{
+    [SerializeField]
+    private List<Sprite> butterflyAppearances = null;
+    [SerializeField]
+    private List<Animation> correspondingAnimations = null;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = butterflyAppearances[Random.Range(0, butterflyAppearances.Count)];
+    }
+}
