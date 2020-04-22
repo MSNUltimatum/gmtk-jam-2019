a0e504bf941b7627ae077b8edbbad8bcf300f7d3
MSNUltimatum <35264174+MSNUltimatum@users.noreply.github.com>
2020-04-22 20:02:20 +0400

    Sniper rifle

---
+++ Assets/Scripts/Skills/BulletModifiers/SniperRifleBulletMod.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SniperRifleBulletMod", menuName = "ScriptableObject/BulletModifier/SniperRifleBulletMod", order = 1)]
+public class SniperRifleBulletMod : BulletModifier
+{
+    public override void SpawnModifier(BulletLife bullet)
+    {
+        var scale = bullet.transform.localScale;
+        scale.y = 0.5f;
+        scale.x = 1.2f;
+        bullet.transform.localScale = scale;
+        base.SpawnModifier(bullet);
+    }
+}
